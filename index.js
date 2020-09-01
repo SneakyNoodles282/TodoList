666b3773902ca02dce120e1a399ffba5e7e5258f
SneakyNoodles282 <clarklcheung@gmail.com>
2020-08-31 20:45:32 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const listItemRouter = require("./routes/list-items")
+app.use('/api', listItemRouter)
+
+app.get('/', (req, res) => {
+    res.send('hi')
+});
+
+app.listen(8000, () => console.log('Hallo'));
+
+
+
+/**
+ * Home
+ * get / 
+ * TodoList
+ * get /api/list-items - get list data (get list)
+ * post /api/list-items/new - add todo to list
+ * DELETE /api/list-items - remove all list data
+ * DELETE /api/list-item/:id - delete by id (listItem in list TodoList)
+ * post /api/list-item/update/:id - check uncheck(add/remove class)
+ */
