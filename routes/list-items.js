14c5f21e6b695c0361537978fe4f685c1f3fdd9e
SneakyNoodles282 <clarklcheung@gmail.com>
2020-09-12 17:11:17 -0400

    validated fields

--- routes/list-items.js
+++ routes/list-items.js
@@ -16,6 +16,7 @@ let todos = [
       "id": "2.3786333",
       "checked": "false"
     }
+
   ];
 
 
@@ -25,8 +26,27 @@ router.get('/list-items', (req, res) => {
 
 
 router.post('/list-items/new', (req, res) => {
-    todos.push(req.body)
-    res.json(todos)
+  const text = req.body.text
+  const id = req.body.id
+  const checked = req.body.checked
+
+
+  const newTodo = {
+    text,
+    id: parseInt(id),
+    checked: checked === 'true'
+  }
+
+  if(!newTodo.text){
+    res.status(400).json({error:"Task Is Empty"})
+    return
+  }
+  if(!newTodo.id|| isNaN(newTodo.id)){
+    res.status(400).json({error:"Invalid ID"})
+    return
+  }
+  todos.push(newTodo)
+  res.json(newTodo)
 });
 
 router.delete('/list-items', (req, res) => {
@@ -36,8 +56,14 @@ router.delete('/list-items', (req, res) => {
 
 router.delete('/list-items/:id', (req, res) =>{
     const id = req.params.id;
+    let deleteTodo = todos.find(todo => todo.id === id)
+    if(deleteTodo === undefined){
+      res.status(404).json({error:"List Item Doesn't Exist"})
+      return
+    }
     todos = todos.filter(todo => todo.id !== id)
     res.json(todos)
+    
 
 })
 
@@ -45,6 +71,11 @@ router.post('/list-items/update/:id', (req,res) =>{
     const id = req.params.id;
     let updatedTodo = todos.find(todo => todo.id === id)
     todos = todos.filter(todo => todo.id !== id)
+    if(updatedTodo === undefined){
+      res.status(404).json({error:"List Item Doesn't Exist"})
+      return
+    } 
+
     updatedTodo.checked = true
     console.log(updatedTodo)
     todos.push(updatedTodo)
