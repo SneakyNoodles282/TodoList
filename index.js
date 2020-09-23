bd5f2e940214522f0635bbd4e7ccad7fc4a92526
SneakyNoodles282 <clarklcheung@gmail.com>
2020-09-22 21:04:19 -0400

    connected with mongodb

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
 const express = require('express');
 const app = express();
 const path = require('path');
 const listItemRouter = require("./routes/list-items")
-
-app.use(express.static('client'))
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use('/api', listItemRouter)
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
-});
+const client = new MongoClient(process.env.MONGO_DB_URI);
 
 
+async function connectClient() {
+    try {
+        await client.connect();
+
+        const database = client.db('todoList');
+        const collection = database.collection('TodoEntry');
+
+    } catch(e) {
+        await client.close();
+        console.log(e)
+    }
+
+    app.use(express.static('client'))
+    app.use(express.json()) // for parsing application/json
+    app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+    app.use('/api', listItemRouter)
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/index.html'));
+    });
 
 
-app.listen(8000, () => console.log('Hallo'));
+
+
+    app.listen(8000, () => console.log('Hallo'));
+
+}
+
+connectClient()
+
 
 
 
